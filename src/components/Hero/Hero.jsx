e9c46a1fcd7df5a9187d046b6e404fe3597dbe84
Moritz Mücke <moritz.muecke@aoe.com>
2020-06-17 00:26:43 +0200

    Adding hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import PortfolioContext from '../../context/context';
 
 const Header = () => {
   const { hero } = useContext(PortfolioContext);
-  const { title, name, subtitle, cta } = hero;
+  const { title, name, subtitle, profession, cta } = hero;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -26,15 +26,15 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            <span className="text-color-main">{name || 'Moritz Mücke'}</span>
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            {subtitle || 'and I\m a'} {profession || 'Software Engineer'}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <a className="cta-btn cta-btn--hero" href="#about">
-              {cta || 'Know more'}
+              {cta || 'Tell me more!'}
             </a>
           </p>
         </Fade>

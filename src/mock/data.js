62d63075ea80b0129d4478a24ac0794ecd7f857f
Moritz Mücke <moritz.muecke@aoe.com>
2020-06-17 00:25:55 +0200

    Adding about section and filling data

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,47 @@ import uuidv1 from 'uuid/v1';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Moritz Mücke | Software Engineer', // e.g: 'Name | Developer'
+  lang: 'de', // e.g: en, es, fr, jp
+  description: 'Portfolio Page of Moritz Mücke', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi, my name is',
+  name: 'Moritz Mücke',
+  subtitle: 'and I\'m a',
+  profession: 'Software Engineer',
+  cta: 'Tell me more!',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'Hi I\'m Moritz, 32 year old software developer from Kassel. In 2016 I finished my B.Sc. degree in Media and Computer Science at the RheinMain University of Applied Sciences. I am working in the IT business since 2005. At the moment I am working for the awesome company',
+  paragraphOneWork: 'AOE GmbH.',
+  paragraphTwo: 'I spent the past years with designing and developing web applications and microservices, doing Identity- and Access Management (IAM) stuff like single sign on and lots of DevOps related work.',
+  paragraphThree: 'Besides my work as a software engineer I\'m organizing a Rock-Music Festval with some friends called Haune-Rock. It takes place in Germany in a small village called Haunetal-Odensachsen.'
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+    img: 'haune-rock.png',
+    title: 'Haune-Rock Festival',
+    info: 'Started as an experiment but has grown to a small to medium-sized Music Festival with bands from all over the nation and also some bands outside of Germany.',
+    info2: 'My tasks besides everything web related like our Homepage, eCommerce and Social Media is also scouting of bands which fits to our audience.',
+    url: 'https://haune-rock.de',
+    repo: '', // if no repo, the button will not show up
+  }
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Wanna talk to me? Go ahead!',
+  btn: 'Mail me!',
+  email: 'hi@moritz-muecke.de',
 };
 
 // FOOTER DATA
@@ -68,27 +51,17 @@ export const footerData = {
     {
       id: uuidv1(),
       name: 'twitter',
-      url: '',
-    },
-    {
-      id: uuidv1(),
-      name: 'codepen',
-      url: '',
+      url: 'https://twitter.com/moritz_muecke',
     },
     {
       id: uuidv1(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/moritz-m%C3%BCcke-375641158/',
     },
     {
       id: uuidv1(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/moritz-muecke',
     },
   ],
-};
-
-// Github start/fork buttons
-export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
 };

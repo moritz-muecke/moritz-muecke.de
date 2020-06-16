ede3808c9b1221939f3f58ec066a9664295cb8ae
Moritz Mücke <moritz.muecke@aoe.com>
2020-06-17 00:27:58 +0200

    Adding haune-rock to projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -32,7 +32,7 @@ const Projects = () => {
 
             return (
               <Row key={index}>
-                <Col lg={4} sm={12}>
+                <Col lg={6} sm={12}>
                   <Fade
                     left={isDesktop}
                     bottom={isMobile}
@@ -71,7 +71,7 @@ const Projects = () => {
                     </div>
                   </Fade>
                 </Col>
-                <Col lg={8} sm={12}>
+                <Col lg={6} sm={12}>
                   <Fade
                     right={isDesktop}
                     bottom={isMobile}
